dd3ef8e1cbf3d1c6b56e3f29faa7520db42413d9
Frank Contrepois <frank@coblan.it>
2025-08-02 15:53:36 +0100

    removing old shell files

--- src/data/region_to_service.csv.sh
+++
@@ -1,1 +0,0 @@
-curl -f http://data.frankcontrepois.com/FinOpsGuyAwsPricingElaboratedData/region_to_service.csv
--- src/data/t3microOdHourPerRegion.csv.sh
+++
@@ -1,1 +0,0 @@
-curl -f http://data.frankcontrepois.com/FinOpsGuyAwsPricingElaboratedData/t3microOdHourPerRegion.csv
